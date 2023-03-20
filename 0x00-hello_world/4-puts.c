a16590ad042ce179ed3141b335892efae9d6300a
chiamakauzodike <chiamakauzodike@users.noreply.github.com>
2023-03-19 17:03:41 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"progamming is like building a multingual puzzle");
+	return (0);
+}
